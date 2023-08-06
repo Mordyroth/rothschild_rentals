01c70cea70f28d38e3340d5d9683508c8bc437a2
Mordyroth <mordy@mrst.co>
2023-08-06 16:30:16 +0000

    Description of changes

--- HyundaiBlueLink/get_odometer.php
+++ HyundaiBlueLink/get_odometer.php
@@ -1,15 +1,6 @@
 <?php
-// Get the VIN from the query string
 $vin = $_GET['vin'];
-
-// Define the command to run the status.js script with Node.js, passing the VIN
 $command = "node HyundaiBlueLink/status.js " . escapeshellarg($vin);
-
-// Execute the command and capture the output
 exec($command, $output);
-// Debugging code
-echo "Command: $command\n";
-print_r($output);
-// Return the odometer reading (assuming it's the first line of the output)
 echo $output[0];
 ?>
--- HyundaiBlueLink/status.js
+++ HyundaiBlueLink/status.js
@@ -1,20 +1,14 @@
-const BlueLinky = require('bluelinky');
+const BlueLinky = require("bluelinky");
 
 const client = new BlueLinky({
-  username: process.env.BLUELINKY_USERNAME || "mordy@rothschildrentals.com",
-  password: process.env.BLUELINKY_PASSWORD || "$Mordyroth0430",
+  username: "mordy@rothschildrentals.com",
+  password: "$Mordyroth0430",
   region: "US",
   pin: "8500",
 });
 
-const vinArg = process.argv[2]; // Get the VIN from the command-line argument
-
-client.on('ready', async () => {
-  const vehicles = await client.getVehicles();
-  vehicles.forEach(async (vehicle) => {
-    if (vehicle.vin === vinArg) { // Check if the VIN matches the argument
-      const status = await vehicle.status();
-      console.log(status.odometer); // Print the odometer reading
-    }
-  });
+client.on("ready", async () => {
+  const vehicle = client.getVehicle(process.argv[2]);
+  const status = await vehicle.status();
+  console.log(status.odometer);
 });
