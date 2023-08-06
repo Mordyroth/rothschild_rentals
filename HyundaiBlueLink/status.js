29c184a302784470ae7ba96e2ba1663132da708f
Mordyroth <39922044+Mordyroth@users.noreply.github.com>
2023-08-06 11:49:30 -0400

    Update status.js

--- HyundaiBlueLink/status.js
+++ HyundaiBlueLink/status.js
@@ -1,16 +1,20 @@
 const BlueLinky = require('bluelinky');
 
 const client = new BlueLinky({
-  username: "mordy@rothschildrentals.com",
-  password: "$Mordyroth0430",
+  username: process.env.BLUELINKY_USERNAME || "mordy@rothschildrentals.com",
+  password: process.env.BLUELINKY_PASSWORD || "$Mordyroth0430",
   region: "US",
   pin: "8500",
 });
 
+const vinArg = process.argv[2]; // Get the VIN from the command-line argument
+
 client.on('ready', async () => {
   const vehicles = await client.getVehicles();
   vehicles.forEach(async (vehicle) => {
-    const status = await vehicle.status();
-    console.log(`Odometer: ${status.odometer}`); // Access the odometer information
+    if (vehicle.vin === vinArg) { // Check if the VIN matches the argument
+      const status = await vehicle.status();
+      console.log(status.odometer); // Print the odometer reading
+    }
   });
 });
